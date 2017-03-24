31ef1bb30683079a3a4069a4747b88b887d81548
Igor Araújo <igor.araujo91@gmail.com>
2017-03-24 08:29:23 +0100

    Adds initial menu.js, index.js, index.html and package.json

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, dialog, Menu }  = require("electron");
+const path                                  = require("path");
+const url                                   = require("url");
+const menuTemplate                          = require("./menu");
+
+let mainWindow      = null;
+
+function createWindow(){
+    mainWindow = new BrowserWindow({ width: 800, height: 600});
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "views/index.html"),
+        protocol: "file:",
+        slashes : true
+    }));
+
+    if(process.argv[2] === "--dev"){
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.on("closed", function(){
+        mainWindow = null;
+    });
+}
+
+app.on("window-all-closed", function(){
+    //For macosx we don't leave the application until the user explicitly do so
+    if(process.platform != "darwin"){
+        app.quit();
+    }
+});
+
+//Create the mainWindow when the application is ready
+app.on("ready", createWindow);
+
+//For OSX, re-create the mainWindow if the docker icon was clicked and no windows exist
+app.on("activate", function(){
+    if(!mainWindow){
+        createWindow();
+    }
+});
+
+Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
---
+++ src/menu.js
@@ -0,0 +1,74 @@
+const template = [
+    {
+        label   : "File",
+        submenu : [
+            { label: "New File"  , accelerator: "CmdOrCtrl+N"           },
+            { type : "separator"                                        },
+            { label: "Open File" , accelerator: "CmdOrCtrl+O"           },
+            { type : "separator"                                        },
+            { label: "Save"      , accelerator: "CmdOrCtrl+S"           },
+            { label: "Save As...", accelerator: "CmdOrCtrl+Shift+S"     },
+            { label: "Save All"                                         },
+            { type : "separator"                                        },
+            { role : "quit"                                             }
+        ]
+    },
+    {
+        label   : "Edit",
+        submenu : [
+            { role: "undo"                },
+            { role: "redo"                },
+            { type: "separator"           },
+            { role: "cut"                 },
+            { role: "copy"                },
+            { role: "paste"               },
+            { role: "pasteandmatchstyle"  },
+            { role: "delete"              },
+            { role: "selectall"           }
+        ]
+    },
+    {
+        label   : "View",
+        submenu : [
+            { role: "resetzoom"         },
+            { role: "zoomin"            },
+            { role: "zoomout"           },
+            { type: "separator"         },
+            { role: "togglefullscreen"  }
+        ]
+    },
+    {
+        role    : "window",
+        submenu : [
+            { role: "minimize"          },
+            { role: "close"             }
+        ]
+    },
+    {
+        role    : "help",
+        submenu : [
+            {
+                label: "Learn More",
+                click () { require("electron").shell.openExternal("https://github.com/araujoigor/apexed") }
+            }
+        ]
+    }
+];
+
+if (process.platform === "darwin") {
+    template.unshift({
+        label   : app.getName(),
+        submenu : [
+            { role: "about"                 },
+            { type: "separator"             },
+            { role: "Preferences"           },
+            { type: "separator"             },
+            { role: "quit"                  }
+        ]
+    });
+} else {
+    template[1].submenu.push({ type : "separator"    });
+    template[1].submenu.push({ label: "Preferences"  });
+}
+
+module.exports = template;
