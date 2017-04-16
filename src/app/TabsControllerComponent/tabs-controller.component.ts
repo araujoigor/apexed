9bc0fda700b059585f0366880d6544530763f243
Igor Araujo <igor.araujo91@gmail.com>
2017-04-16 15:45:34 +0200

    Added close tab handler for tabs-controller

--- src/app/TabsControllerComponent/tabs-controller.component.ts
+++ src/app/TabsControllerComponent/tabs-controller.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: "tabs-controller",
-    templateUrl: "./tabs-controller.component.html",
-    styleUrls: ["./tabs-controller.component.css"]
+    selector    : "tabs-controller",
+    templateUrl : "./tabs-controller.component.html",
+    styleUrls   : [ "./tabs-controller.component.css" ]
 })
 export class TabsControllerComponent{
     tabs : string[] = ["Teste"];
+
+    public closeTab(tab: string){
+        let index = this.tabs.indexOf(tab);
+
+        if(index >= 0){
+            this.tabs.splice(index, 1);
+        }
+    }
 }
